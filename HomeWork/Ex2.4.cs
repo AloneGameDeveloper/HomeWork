b8a5a0eef786d4e3d5b4c141965c49bce0559f7a
darius <darius.gagim@hotmail.com>
2020-08-31 23:20:33 +0300

    HomeWork.2

---
+++ HomeWork/Ex2.4.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace HomeWork2._4
+{
+    class Program
+    {
+
+        /* Реализовать метод проверки логина и пароля. На вход метода подается логин и пароль. 
+         * На выходе истина, если прошел авторизацию, и ложь, если не прошел (Логин: root, Password: GeekBrains). 
+         * Используя метод проверки логина и пароля, написать программу: пользователь вводит логин и пароль, 
+         * программа пропускает его дальше или не пропускает. С помощью цикла do while ограничить ввод пароля тремя попытками.
+*/
+
+        static void Main(string[] args)
+        {
+
+           
+            string login , password;
+            int a = 0;
+            
+                do
+                {
+                    Console.WriteLine("Input Login : "); login = Console.ReadLine();
+                    Console.WriteLine("Input Password : "); password = Console.ReadLine();
+
+                    if (login == "root" || password == "GeekBrains")
+                    {
+                        Console.WriteLine("Succes");
+                    a = 10;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error");
+                        a++;
+                    }
+                }
+                while (a < 3);
+            
+            
+            
+        }
+    }
+}
